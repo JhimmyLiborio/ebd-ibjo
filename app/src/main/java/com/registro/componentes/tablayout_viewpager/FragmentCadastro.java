08c6884cee463b1d702f8c271a48772fc43396c6
Jhimmy <liboriojhimmy@gmail.com>
2020-11-04 21:47:54 -0300

    Construção da Tela de Cadastro

--- app/src/main/java/com/registro/componentes/tablayout_viewpager/FragmentCadastro.java
+++ app/src/main/java/com/registro/componentes/tablayout_viewpager/FragmentCadastro.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 
 /**
@@ -15,6 +17,9 @@ import android.view.ViewGroup;
  */
 public class FragmentCadastro extends Fragment {
 
+    // itens para o Menu: "dropdown_menu_categoria_classe.xml"
+    private String [] categoriaClasse =  {"Crianças", "Adolescente", "Jovens",
+            "Mulheres", "Homens"};
 
     public FragmentCadastro() {
         // Required empty public constructor
@@ -25,7 +30,20 @@ public class FragmentCadastro extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate( R.layout.fragment_fragment_cadastro, container, false );
+        View view =  inflater.inflate( R.layout.fragment_fragment_cadastro, container,
+                false );
+
+
+        // Associa os itens do Array "categoriaClasse" ao layout dropdown_menu_categoria_classe.xml
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
+                R.layout.dropdown_menu_categoria_classe,
+                categoriaClasse);
+
+        //Seta a associação no compomente AutoCompleteTextView
+        AutoCompleteTextView categoriasMenu =  view.findViewById(R.id.autoCompleteDropdownMenu);
+        categoriasMenu.setAdapter(adapter);
+
+        return view;
     }
 
 }
