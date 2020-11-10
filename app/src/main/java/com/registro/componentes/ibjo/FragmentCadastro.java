7cacde80d4ec2ee0c0d5b43afc5cee0d96bee13b
Jhimmy <liboriojhimmy@gmail.com>
2020-11-10 12:27:51 -0300

    Ativando ação do botação cadastrar

--- app/src/main/java/com/registro/componentes/ibjo/FragmentCadastro.java
+++ app/src/main/java/com/registro/componentes/ibjo/FragmentCadastro.java
@@ -11,6 +11,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,8 +24,18 @@ import android.widget.AutoCompleteTextView;
 public class FragmentCadastro extends Fragment {
 
     // itens para o Menu: "dropdown_menu_categoria_classe.xml"
-    private String [] categoriaClasse =  {"Crianças", "Adolescente", "Jovens",
+    private final String [] categoriaClasse =  {"Crianças", "Adolescente", "Jovens",
             "Mulheres", "Homens"};
+
+    // campos de informação: nome  e classe
+    private TextInputLayout inputLayoutNome;
+    private TextInputLayout inputLayoutClasse;
+
+    private TextInputEditText textNome;
+    private AutoCompleteTextView textClasse;
+
+    private MaterialButton buttonCadastrar;
+
 
     public FragmentCadastro() {
         // Required empty public constructor
@@ -29,21 +45,41 @@ public class FragmentCadastro extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view =  inflater.inflate( R.layout.fragment_fragment_cadastro, container,
-                false );
+            // Inflate the layout for this fragment
+            View view =  inflater.inflate( R.layout.fragment_fragment_cadastro, container,
+                    false );
 
+            inputLayoutNome = view.findViewById(R.id.textInputLayoutNome);
+            inputLayoutClasse = view.findViewById(R.id.textInputLayoutClasse);
+            buttonCadastrar = view.findViewById(R.id.buttonCadastrar);
 
-        // Associa os itens do Array "categoriaClasse" ao layout dropdown_menu_categoria_classe.xml
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
-                R.layout.dropdown_menu_categoria_classe,
-                categoriaClasse);
+            // Associa os itens do Array "categoriaClasse" ao layout dropdown_menu_categoria_classe.xml
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
+                    R.layout.dropdown_menu_categoria_classe,
+                    categoriaClasse);
 
-        //Seta a associação no compomente AutoCompleteTextView
-        AutoCompleteTextView categoriasMenu =  view.findViewById(R.id.autoCompleteDropdownMenu);
-        categoriasMenu.setAdapter(adapter);
+            //Seta a associação no compomente AutoCompleteTextView
+            textClasse =  view.findViewById(R.id.autoCompleteDropdownMenu);
+            textClasse.setAdapter(adapter);
 
-        return view;
+            
+            //dispara ação do click "botão cadastrar"
+            buttonCadastrar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make( v.getRootView(), R.string.tab3, BaseTransientBottomBar.LENGTH_LONG)
+                            .show();
+                }
+            });
+
+            return view;
     }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
 
 }
