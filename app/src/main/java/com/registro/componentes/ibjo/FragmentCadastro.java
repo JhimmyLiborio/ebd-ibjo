63300fbb9e5d2f63c7841902b39ad8a485a066c2
Jhimmy <liboriojhimmy@gmail.com>
2020-11-10 23:32:22 -0300

    Tratamento e verificação de campos do cadastro

--- app/src/main/java/com/registro/componentes/ibjo/FragmentCadastro.java
+++ app/src/main/java/com/registro/componentes/ibjo/FragmentCadastro.java
@@ -2,14 +2,13 @@ package com.registro.componentes.ibjo;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
@@ -17,11 +16,16 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentCadastro extends Fragment {
+
 
     // itens para o Menu: "dropdown_menu_categoria_classe.xml"
     private final String [] categoriaClasse =  {"Crianças", "Adolescente", "Jovens",
@@ -34,47 +38,122 @@ public class FragmentCadastro extends Fragment {
     private TextInputEditText textNome;
     private AutoCompleteTextView textClasse;
 
-    private MaterialButton buttonCadastrar;
 
-
+    // Construtor público vázio requerido para Firebase
     public FragmentCadastro() {
-        // Required empty public constructor
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-            // Inflate the layout for this fragment
-            View view =  inflater.inflate( R.layout.fragment_fragment_cadastro, container,
-                    false );
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate( R.layout.fragment_fragment_cadastro, container,
+                false );
 
-            inputLayoutNome = view.findViewById(R.id.textInputLayoutNome);
-            inputLayoutClasse = view.findViewById(R.id.textInputLayoutClasse);
-            buttonCadastrar = view.findViewById(R.id.buttonCadastrar);
+        inputLayoutNome = view.findViewById(R.id.textInputLayoutNome);
+        textNome = view.findViewById(R.id.textInputNome);
 
-            // Associa os itens do Array "categoriaClasse" ao layout dropdown_menu_categoria_classe.xml
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
-                    R.layout.dropdown_menu_categoria_classe,
-                    categoriaClasse);
+        inputLayoutClasse = view.findViewById(R.id.textInputLayoutClasse);
+        MaterialButton buttonCadastrar = view.findViewById(R.id.buttonCadastrar);
 
-            //Seta a associação no compomente AutoCompleteTextView
-            textClasse =  view.findViewById(R.id.autoCompleteDropdownMenu);
-            textClasse.setAdapter(adapter);
+        // Associa os itens do Array "categoriaClasse" ao layout dropdown_menu_categoria_classe.xml
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
+                R.layout.dropdown_menu_categoria_classe,
+                categoriaClasse);
 
-            
-            //dispara ação do click "botão cadastrar"
-            buttonCadastrar.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Snackbar.make( v.getRootView(), R.string.tab3, BaseTransientBottomBar.LENGTH_LONG)
-                            .show();
-                }
-            });
+        //Seta a associação no compomente AutoCompleteTextView
+        textClasse =  view.findViewById(R.id.autoCompleteDropdownMenu);
+        textClasse.setAdapter(adapter);
 
-            return view;
+        //dispara ação do click "botão cadastrar"
+        buttonCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checarCampos();
+            }
+        });
+
+        return view;
     }
 
+
+    // verifica se os campos estão preenchidos corretamente
+    private void checarCampos(){
+
+        setErroCampo(0, null, false);
+
+        if ( Objects.requireNonNull(textNome.getText()).toString().isEmpty() )
+            setErroCampo(1, "Digite o nome do Aluno!", true);
+
+        verificarPadraoPreenchimentoNome();
+
+        if ( textClasse.getText().toString().isEmpty() )
+            setErroCampo(2, "Selecione a Classe do Aluno!", true);
+
+        // sem erros para proseguir
+        if(!inputLayoutClasse.isErrorEnabled()
+                && !inputLayoutNome.isErrorEnabled()){
+             cadastrarAluno();
+             limparCampos();
+        }
+    }
+
+
+    // mostrar erros nos campos sem preenchimento
+    private void setErroCampo(int idCampo, String msgError, boolean encontrado){
+
+        switch (idCampo){
+            case 1:
+                inputLayoutNome.setErrorEnabled(encontrado);
+                inputLayoutNome.setError(msgError);
+                break;
+            case 2:
+                inputLayoutClasse.setError(msgError);
+                inputLayoutClasse.setErrorEnabled(encontrado);
+                break;
+            default: // deixa os campos sem informação de erro a cada verificação
+                inputLayoutNome.setErrorEnabled(encontrado);
+                inputLayoutClasse.setErrorEnabled(encontrado);
+        }
+    }
+
+
+    // Não aceita números, caracteres especiais e menos de 8 letras
+    private void verificarPadraoPreenchimentoNome(){
+
+        Pattern padrao = Pattern.compile("[^a-zA-Z\\s]",
+                Pattern.CASE_INSENSITIVE);
+        Matcher matcher = padrao.matcher(Objects.requireNonNull(textNome.getText()).toString());
+
+        if(matcher.find())
+            setErroCampo(1,
+                    "Digite o nome completo, utilize letras.", true);
+
+        if (textNome.getText().toString().length() < 10)
+            setErroCampo(1,
+                    "Por favor, informe o nome completo.", true);
+    }
+
+
+    // limpar campo após realizar cadastro
+    private void limparCampos(){
+
+        textNome.setText(null);
+        textClasse.setText(null);
+
+        inputLayoutNome.setErrorEnabled(false);
+        inputLayoutNome.setErrorEnabled(false);
+    }
+
+
+    // cadastrar aluno
+    // TODO: fazer conexao com o banco de dados.
+    private void cadastrarAluno(){
+        Snackbar.make(requireView(), "Pronto para cadastrar",
+                BaseTransientBottomBar.LENGTH_LONG).show();
+    }
 
     @Override
     public void onStart() {
